b47b61dfaf606bc1d30ab8e56d2cf2c2a0223775
Allan Espinosa <allan.espinosa@outlook.com>
2015-03-01 00:31:48 +0900

    Write failing test for deploying the app

--- demo/cookbook/test/integration/app/minitest/test_app.rb
+++ demo/cookbook/test/integration/app/minitest/test_app.rb
@@ -4,4 +4,9 @@ class AppTest < MiniTest::Unit::TestCase
   def test_ruby_should_be_installed
     assert File.exists?('/usr/local/bin/ruby'), 'ruby is not installed'
   end
+
+  def test_rackup_should_be_running
+    rackup_pid = File.read('/usr/local/opt/hello/hello.pid').to_i
+    assert_equal 1, Process.kill(0, rackup_pid), 'hello rack app is not running'
+  end
 end
